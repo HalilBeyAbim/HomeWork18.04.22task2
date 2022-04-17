d696ff78e419c4686a1417cfafbe6ff9478951f4
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-04-17 16:58:30 +0400

    added second task

---
+++ homeWork2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 20;//bir deyer veririk
+            int dividednumber = 7;//ve bir bolunen deyer elave edirik
+            int remainder = number % dividednumber; //bir qaliq yaradiriq
+            if (remainder==0 ) //eger qaliq beraberdirse 0'a her sey hazirdir demekdi
+            {
+                Console.WriteLine("bolunur");
+            }
+
+            else if(remainder<dividednumber/2) // qaliqin ortasini tapiriq
+            {
+                
+                Console.WriteLine("bolunmur, bolunen en yaxin reqem:  " + (number-remainder));// en yaxin asaqi deyeri ortaya cixardiriq
+            }
+            else if (remainder == dividednumber / 2)
+            {
+                Console.WriteLine("eded iki terefin ortasindadir");// deyer eger iki deyerin ortadirsa 
+            }
+            else
+            {
+                Console.WriteLine("bolunmur, bolunen en yaxin reqem:  " + (number + dividednumber - remainder));//en yaxin ust deyerini ortaya cixardiriq
+            }
+        }
+    }
+}
